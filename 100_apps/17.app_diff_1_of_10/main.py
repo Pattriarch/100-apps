c1947b64d6ef3020d59224457a1ad5f0cdc5211f
Daniil <pgelo298@mail.ru>
2021-09-18 01:08:03 +0300

    Initital commit

---
+++ 100_apps/17.app_diff_1_of_10/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backward():
+    tim.backward(10)
+
+def clockwise():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def counter_clockwise():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.up()
+    tim.home()
+    tim.down()
+
+screen.listen()
+screen.onkey(key='w', fun=move_forwards)
+screen.onkey(key='s', fun=move_backward)
+screen.onkey(key='a', fun=counter_clockwise)
+screen.onkey(key='d', fun=clockwise)
+screen.onkey(key='c', fun=clear)
+screen.exitonclick()
